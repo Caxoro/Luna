2e026fbc7067c53d778a606fcba73b664b520c84
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-07-31 19:22:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,6 @@ st.title("🌙 Bem vindo ao chat Luna")
 mensagem = st.chat_input("Digite sua mensagem para Luna")
 container = st.container(height=600)
 with container:
-    st.write("Historico da conversa.")
-if mensagem != "":
     st.session_state.messages.append(mensagem)
     for mens in st.session_state.messages:
         container.chat_message("user").write(mens)
