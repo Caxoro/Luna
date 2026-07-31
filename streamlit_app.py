1f9099f6cb6d8087f3abea083958ee673fa75f15
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-07-31 20:58:40 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,11 +18,11 @@ if mensagem:
     st.session_state.messages.append(mensagem)
     for mens in st.session_state.messages:
         st.chat_message("user").write(mens)
-    interaction = client.interactions.create(
-        model="gemini-3.5-flash", 
-        input=mens,
-        previous_interaction_id=previous_id,
-    )
+        interaction = client.interactions.create(
+            model="gemini-3.5-flash", 
+            input=mens,
+            previous_interaction_id=previous_id,
+        )
     for ai in st.session_state.luna_messages:
         st.session_state.luna_messages.append(interaction.output_text)
         st.chat_message("ai").write(interaction.output_text[0])
