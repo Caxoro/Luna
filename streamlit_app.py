8ff323f8eb7d804b42db431114cc7a7f42dc4d5d
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-07-31 15:51:11 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,9 @@ messages = []
 st.title("🌙 Bem vindo ao chat Luna")
 container = st.container(height=500)
 mensagem = ""
-mensagem = st.chat_input("Digite sua mensagem para Luna")
-if mensagem != "":
-    mensagem += messages.append(mensagem)
-    container.chat_message("user").write(mensagem)
+
+while True:
+    mensagem = st.chat_input("Digite sua mensagem para Luna")
+    if mensagem != "":
+        messages.append(mensagem)
+        container.chat_message("user").write(mensagem)
