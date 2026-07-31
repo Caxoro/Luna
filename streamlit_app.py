4b1268c488bcf9454a0f1ad6b1e746ac3cc682eb
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-07-31 14:17:17 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🌙 Bem vindo ao chat Luna")
+mensagem = st.chat_input("Digite sua mensagem para Luna")
