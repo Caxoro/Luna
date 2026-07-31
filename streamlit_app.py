38d38fdb0bb68d14ecdc9fd8de62b07605f2451b
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-07-31 19:25:37 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,10 @@ import streamlit as st
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-st.title("🌙 Bem vindo ao chat Luna")
+st.title("🌙 Luna - Assitente Virtual")
 mensagem = st.chat_input("Digite sua mensagem para Luna")
-container = st.container(height=600)
 st.session_state.messages.append(mensagem)
 for mens in st.session_state.messages:
-    container.chat_message("user").write(mens)
+    st.write(mens)
     
     
