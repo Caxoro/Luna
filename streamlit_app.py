1af56adfd43a65522a3e7dda4baad0ada25ac58f
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-07-31 19:12:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,11 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 
 st.title("🌙 Bem vindo ao chat Luna")
-container = st.container(height=600)
-mensagem = st.chat_input("Digite sua mensagem para Luna")
-
-if mensagem != "":
-    
-    st.session_state.messages.append(mensagem)
-    for mens in st.session_state.messages:
-        container.chat_message("user").write(mens)
+with container = st.container(height=600)
+    mensagem = st.chat_input("Digite sua mensagem para Luna")
+    if mensagem != "":
+        st.session_state.messages.append(mensagem)
+        for mens in st.session_state.messages:
+            container.chat_message("user").write(mens)
     
     
