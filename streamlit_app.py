aae3fe6c9a22cd0c59272cbde4fc77384f3d6501
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-08-01 09:51:43 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,16 +18,17 @@ if "ai" not in st.session_state:
 
 st.title("🌙 Luna - Assitente Virtual")
 
-if mensagem:
-     st.session_state.messages.append(mensagem)
-     st.session_state.mensagem = mensagem
-     st.chat_message("user").write(st.session_state.mensagem)
-     interaction = client.interactions.create(
-        model="gemini-3.5-flash", 
-        input=mensagem,
-        previous_interaction_id=previous_id,
-     )
-     st.session_state.messages.append(interaction.output_text)
-     st.chat_message("ai").write(st.session_state.ai, key="ai")
+with st.chat_message:
+    if mensagem:
+        st.session_state.messages.append(mensagem)
+        st.session_state.mensagem = mensagem
+        st.chat_message("user").write(st.session_state.mensagem)
+        interaction = client.interactions.create(
+            model="gemini-3.5-flash", 
+            input=mensagem,
+            previous_interaction_id=previous_id,
+        )
+        st.session_state.messages.append(interaction.output_text)
+        st.chat_message("ai").write(st.session_state.ai, key="ai")
     
     
