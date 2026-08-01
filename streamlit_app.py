5567cd2f17fb1cbc71fdb0ac036627ffb6db5a3e
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-07-31 21:11:40 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ if mensagem:
             input=mens,
             previous_interaction_id=previous_id,
         )
-        st.session_state.messages.append(interaction.output_text)
+        st.session_state.luna_messages.append(interaction.output_text)
     for ai in st.session_state.luna_messages:
         st.chat_message("ai").write(interaction.output_text)
     
