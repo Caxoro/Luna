ef721d89caae6d2654dd03b52c80ccb49a395ce7
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-08-03 16:17:27 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ if mensagem:
         st.session_state.historico.append({"type": "user_input","content": [{"type": "text", "text": mensagem}]})
     with st.chat_message("ai"):
         interaction = client.interactions.create(
-            model="gemini-3.6-flash",
+            model="gemini-2.5-flash",
             system_instruction="Voce é luna uma assistente virtual gentil e bem humana, voce gosta da cor azul, e seu criador se chama Emerson"
             "voce pode responder de maneira informal e usar girias, evite escrever textos muito longos e sempre que possivel resuma boa parte das respostas",
             store=False,
