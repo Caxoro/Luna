da75c363e4d8bdd65eefa9728d07ea316a4f72f8
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-08-03 13:55:28 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,8 @@ if mensagem:
         st.write(mensagem)
     st.session_state.messages.append({"role": "user","content": mensagem})
     for m in st.session_state.messages:
-            memoria = (f"{m["role"]}: {m["content"]}")
+        memoria = (f"{m["role"]}: {m["content"]}")
+        st.write(memoria)
     with st.chat_message("ai"):
         interaction = client.interactions.create(
             model="gemini-3.5-flash",
