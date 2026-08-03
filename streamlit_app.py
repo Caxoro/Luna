23daee12fb9a19679671abb698432569c4cba927
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-08-03 09:09:33 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,8 @@ mensagem = st.chat_input("Digite sua mensagem para Luna")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
-if "historico" not in st.session_state:
-    st.session_state.historico = []
+
+historico = []
 
 for h in st.session_state.messages:
     with st.chat_message(h["role"]):
@@ -25,11 +25,11 @@ if mensagem:
 
     with st.chat_message("ai"):
        for i in st.session_state.messages:
-           st.session_state.historico.append(i)
+           historico.append(i)
        interaction = client.interactions.create(
            model="gemini-3.6-flash",
            store=False,
-           input=st.session_state.historico.value(),
+           input=historico,
        )
        st.write(interaction.output_text)
     st.session_state.messages.append({"role": "ai","content": interaction.output_text})
