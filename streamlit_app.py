03e038f8d46f9635f486d8425b54805185157066
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-08-03 08:46:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ if mensagem:
        interaction = client.interactions.create(
            model="gemini-3.6-flash",
            store=False,
-           input="messages",
+           input=st.write(st.session_state.messages),
        )
        st.write(interaction.output_text)
     st.session_state.messages.append({"role": "ai","content": interaction.output_text})
