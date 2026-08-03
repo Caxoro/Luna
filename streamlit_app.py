bd9e48b0a3c449d6177d9c74240552da49c0cd41
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-08-03 08:41:42 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ if mensagem:
     with st.chat_message("ai"):
        interaction = client.interactions.create(
            model="gemini-3.6-flash",
-           store=False
+           store=false,
            input=historico,
        )
        st.write(interaction.output_text)
