0b2384b07305f03a290a0e453f405adb27ea04b0
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-08-03 17:01:31 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ if "historico" not in st.session_state:
 for h in st.session_state.messages:
     with st.chat_message(h["role"]):
         st.write(h["content"])
-st.write(mensagem["files"]["name"])
+st.write(mensagem["files"][0])
 
 if mensagem:
     with st.chat_message("user"):
