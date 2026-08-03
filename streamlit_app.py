d1aa13284395e8fdc5bc373c8e5910b34dd3d912
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-08-03 15:08:41 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,8 @@ if mensagem:
     with st.chat_message("ai"):
         interaction = client.interactions.create(
             model="gemini-3.6-flash",
+            system_instruction="Voce é luna uma assistente virtual gentil e bem humana, voce gosta da cor azul, e seu criador se chama Emerson",
+            "voce pode responder de maneira informal e usar girias, evite escrever textos muito longos e sempre que possivel resuma boa parte das respostas",
             store=False,
             input=st.session_state.historico,
         )
