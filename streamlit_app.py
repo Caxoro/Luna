32d6043dc02d26493acec1aa6626106be9d03414
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-08-04 14:18:55 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ for h in st.session_state.messages:
 if mensagem:
     with st.chat_message("user"):
         st.write("Usuário: ",mensagem["text"])
-        st.write(mensagem.values())
+        st.write(mensagem.get)
         st.session_state.messages.append({"role": "user","content": mensagem})
         st.session_state.historico.append({"type": "user_input","content": [{"type": "text", "text": mensagem}]})
         if mensagem["files"] != None:
