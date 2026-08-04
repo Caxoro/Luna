f9be4f15ef11e9fabe323c54bcbddc97708a80a1
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-08-04 11:15:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ if mensagem:
         st.session_state.historico.append({"type": "user_input","content": [{"type": "text", "text": mensagem}]})
         if mensagem["files"] != None:
             imagem = mensagem["files"][0]
-            arquivo = client.files.upload(file=imagem.upload_url)
+            arquivo = client.files.upload(file=imagem.name)
             st.session_state.historico.append({"type": "image", "uri": arquivo.uri, "mime_type": arquivo.mime_type})
         else:
             mensagem["files"] = None
