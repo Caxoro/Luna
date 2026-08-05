37f8e0fecb08822cf5a5fe559d958a37639da328
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-08-05 10:06:14 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ if mensagem:
         st.session_state.historico.append({"type": "user_input","content": [{"type": "text", "text": mensagem["text"]}]})
         if mensagem["files"] != None:
             imagem = mensagem.files
-            bytes_imagem = imagem.getvalue()
+            bytes_imagem = imagem[0].getvalue()
             tipo_imagem = str(imagem.type)
             image_b64 = base64.b64encode(bytes_imagem).decode("utf-8")
       
