e2a38908fd885ca69593ae176ad326cd053fd5eb
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-08-05 10:07:41 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,11 +26,11 @@ for h in st.session_state.messages:
 if mensagem:
     with st.chat_message("user"):
         st.write("Usuário: ",mensagem["text"])
-        st.session_state.messages.append({"role": "user","content": mensagem["text"]})
-        st.session_state.historico.append({"type": "user_input","content": [{"type": "text", "text": mensagem["text"]}]})
+        st.session_state.messages.append({"role": "user","content": mensagem.text})
+        st.session_state.historico.append({"type": "user_input","content": [{"type": "text", "text": mensagem.text}]})
         if mensagem["files"] != None:
-            imagem = mensagem.files
-            bytes_imagem = imagem[0].getvalue()
+            imagem = mensagem.files[0]
+            bytes_imagem = imagem.getvalue()
             tipo_imagem = str(imagem.type)
             image_b64 = base64.b64encode(bytes_imagem).decode("utf-8")
       
