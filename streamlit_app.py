07803b1addf05be62fdcb14e7adc8bfcb0c469d5
Caxoro <95001719+Caxoro@users.noreply.github.com>
2026-08-18 18:43:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,6 @@ client = genai.Client(api_key=chave)
 
 mensagem = st.chat_input("Digite sua mensagem para Luna",
                          accept_file=True,
-                         submit_mode="disabled",
                          file_type=["jpg", "jpeg", "png"],)
 
 if "messages" not in st.session_state:
